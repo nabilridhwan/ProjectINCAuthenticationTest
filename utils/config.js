f8dfd43dca89657166b39294276a4335d1a77a7c
Nabil Ridhwan <nabridhwan@gmail.com>
2022-04-24 11:25:02 +0800

    feat: :sparkles: Moved env vars to config.js file

---
+++ utils/config.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+const CONFIG = {
+    PORT: process.env.PORT || 3000,
+    DB_CONNECTION: process.env.PG_CONNECTION,
+    COOKIE_SECRET: process.env.COOKIE_SECRET,
+    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_SECRET_REGISTERING_USER: process.env.JWT_SECRET_REGISTERING_USER,
+};
+
+module.exports = CONFIG;
